3594f7832b492ea3fdb76132bb48f0a667a70992
donghwa95\donghwa18 <donghwa18@naver.com>
2018-04-23 22:02:15 +0900

    인터넷 강좌 들었던 것 정리 자료

---
+++ a.rb
@@ -0,0 +1,9 @@
+b = ["vegimeal", "lemona", "ricecake", "spicy ramen"]
+
+puts (1..45).to_a.sample(6)
+1.upto(3) do
+ b.each do |a|
+	puts a
+  end
+end
+puts "DONG HWA HAM".upcase
---
+++ b.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+
+require 'sinatra'
+require "sinatra/reloader"
+
+get '/' do
+	 "<h1>You mine a block</h1>"
+end
